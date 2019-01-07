0e34eda713d08e5ec535a6c5a5642e44b4caf4d7
raghu_veerappa <raghu.patrick@gmail.com>
2019-01-07 12:09:32 +0530

    remove these files

---
+++ Primefaces-Hibernate-Spring-Integration-Sample - Copy/src/main/java/com/journaldev/filter/AuthorizationFilter.java
@@ -0,0 +1,53 @@
+package com.journaldev.filter;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class AuthorizationFilter implements Filter {
+
+	public AuthorizationFilter() {
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+
+			HttpServletRequest reqt = (HttpServletRequest) request;
+	        HttpServletResponse resp = (HttpServletResponse) response;
+	        HttpSession session = reqt.getSession(false);
+			String reqURI = reqt.getRequestURI();
+			
+			if (reqURI.indexOf("/login.xhtml") >= 0
+					|| (session != null && session.getAttribute("user") != null))
+				
+			{	
+				chain.doFilter(request, response);
+			}else
+			{
+					resp.sendRedirect(reqt.getContextPath() + "/faces/pages/login.xhtml");	
+		}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
---
+++ Primefaces-Hibernate-Spring-Integration-Sample - Copy/src/main/java/com/journaldev/spring/daoImpl/UserWizardDaoImpl.java
@@ -0,0 +1,125 @@
+package com.journaldev.spring.daoImpl;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.journaldev.hibernate.entity.Role;
+import com.journaldev.hibernate.entity.Tagging;
+import com.journaldev.hibernate.entity.User;
+import com.journaldev.spring.dao.UserWizardDao;
+
+@Repository
+public class UserWizardDaoImpl implements UserWizardDao {
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	@Transactional
+	public void register(User user) throws Exception {
+		// Acquire session
+		Session session = sessionFactory.getCurrentSession();
+		// Save employee, saving behavior get done in a transactional manner
+		session.saveOrUpdate(user);		
+	}
+
+	@Override
+	@Transactional
+	public List<User> getUserList() throws Exception {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		Criteria cr = session.createCriteria(User.class).setFetchMode("tagging",  FetchMode.JOIN);
+		List<User> results = cr.list();
+		return results;
+	}
+	
+	
+	
+	@Override
+	@Transactional
+	public List<Role> getRoleList() throws Exception {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		Criteria cr = session.createCriteria(Role.class);
+		List<Role> results = cr.list();
+		return results;
+	}
+
+	
+	@Override
+	@Transactional
+	public void saveRole(Role role) throws Exception {
+		// Acquire session
+		Session session = sessionFactory.getCurrentSession();
+		// Save employee, saving behavior get done in a transactional manner
+		session.saveOrUpdate(role);		
+	}
+	
+	
+	@Override
+	@Transactional
+	public void deleteUser(User user) throws Exception {
+		// Acquire session
+		Session session = sessionFactory.getCurrentSession();
+		// Save employee, saving behavior get done in a transactional manner
+		session.delete(user);		
+	}
+	
+	@Override
+	@Transactional
+	public void deleteRole(Role role) throws Exception {
+		// Acquire session
+		Session session = sessionFactory.getCurrentSession();
+		// Save employee, saving behavior get done in a transactional manner
+		session.delete(role);		
+	}
+	
+	
+	@Override
+	@Transactional
+	public List<Tagging> getTaggingList() throws Exception {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		Criteria cr = session.createCriteria(Tagging.class);
+		List<Tagging> results = cr.list();
+		return results;
+	}
+	
+	
+	@Override
+	@Transactional
+	public void saveTagging(Tagging tag) throws Exception {
+		// Acquire session
+		Session session = sessionFactory.getCurrentSession();
+		// Save employee, saving behavior get done in a transactional manner
+		session.saveOrUpdate(tag);		
+	}
+
+	
+	
+	@Override
+	@Transactional
+	public void deleteTagging(Tagging tag) throws Exception {
+		// Acquire session
+		Session session = sessionFactory.getCurrentSession();
+		// Save employee, saving behavior get done in a transactional manner
+		session.delete(tag);		
+	}
+	
+	
+	
+}
